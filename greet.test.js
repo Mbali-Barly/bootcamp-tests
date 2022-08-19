4c845f6c35daef56ae1f9d194bf36c3adec95827
Mbali-Barly <mbalih15.marake@gmail.com>
2022-08-19 09:08:34 +0200

    bootcamp tests

---
+++ greet.test.js
@@ -0,0 +1,11 @@
+function greet(Janie) {
+    return 'Hello, ' +  Janie
+   }
+   const msg = greet('Xola')
+   console.log(msg)
+ 
+   const message = greet('Mbali')
+   console.log(message)
+
+   const messagee = greet('Tsholo')
+   console.log(messagee)
